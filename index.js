996ddc371e1d383225cba6cddcdfff744e8aadb0
Teddy <ochiengnyateya@gmail.com>
2022-03-07 23:43:26 +0300

    This commit adds variables to the page.

---
+++ index.js
@@ -0,0 +1,35 @@
+const male = document.getElementById("maleRadio");
+const female = document.getElementById("femaleRadio");
+let title = document.getElementById("title");
+let text = document.getElementById("text");
+let tDate = document.getElementById("dateInput")
+
+let CC;
+let DD;
+let YY;
+let MM;
+let day;
+let d;
+
+
+let akanFemale = [
+    { english: "Sunday", ghana: "Akosua" },
+    { english: "Monday", ghana: "Adwoa" },
+    { english: "Tuesday", ghana: "Abenaa" },
+    { english: "Wednesday", ghana: "Akua" },
+    { english: "Thursday", ghana: "Yaa" },
+    { english: "Friday", ghana: "Afua" },
+    { english: "Saturday", ghana: "Ama" },
+  ];
+  
+  let akanMale = [
+    { english: "Sunday", ghana: "Kwasi" },
+    { english: "Monday", ghana: "Kwadwo" },
+    { english: "Tuesday", ghana: "Kwabena" },
+    { english: "Wednesday", ghana: "Kwaku" },
+    { english: "Thursday", ghana: "Yaw" },
+    { english: "Friday", ghana: "Kofi" },
+    { english: "Saturday", ghana: "Kwame" },
+  ];
+  
+  
