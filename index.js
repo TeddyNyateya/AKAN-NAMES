0ef39632c97ee21eebb88c9000cbe617fadff9e5
Teddy <ochiengnyateya@gmail.com>
2022-03-07 23:48:11 +0300

    This commit adds functions that fetches a users Akan name.

--- index.js
+++ index.js
@@ -67,3 +67,69 @@ let akanFemale = [
     });
   };
   
+ // Formulae for calculating day of the month
+
+const dayOfTheWeek = () => {
+
+    d = (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+    d = Math.floor(d);
+    // d = d + 2;
+    console.log("day of the formulae " + d);
+  
+  
+    if (male.checked) {
+      akanMale.find((item, index, arr) => {
+        if (index == day) {
+          console.log(item.ghana, item.english);
+          title.innerHTML = item.ghana;
+          document.getElementById(
+            "text"
+          ).innerHTML = `Means an African male 👨🏿 born on ${item.english}`;
+        }
+      });
+    } else if (female.checked) {
+      akanMale.find((item, index, arr) => {
+        if (index == day) {
+          console.log(item.ghana, item.english);
+          document.getElementById("title").innerHTML = item.ghana;
+          document.getElementById(
+            "text"
+          ).innerHTML = `Means an African female 👧🏿 born on ${item.english}`;
+        }
+      });
+    }
+  };
+  
+  const validate = () => {
+    if (((male.checked)  == false) && ((female.checked) == false)) {
+      document.getElementById("messageOne").innerHTML =
+        "Select your gender";
+    }
+    else if ( CC == null ) {
+      document.getElementById("messageOne").innerHTML =
+        "Fill in your date";
+    }else if ( ((((male.checked)  == false) && ((female.checked) == false))) || (CC == null) ) {
+      document.getElementById("messageOne").innerHTML =
+        "Fill your date and Gender";
+    }else{
+      document.querySelector(".form").style.display = "none";
+      document.querySelector(".display").style.display = "block";
+      dayOfTheWeek();
+    }
+  
+    let isValidDate = Date.parse("03/17/2021");
+  
+    if (isNaN(isValidDate)) {
+      document.getElementById("messageOne").innerHTML =
+        "This is not a valid date format.";
+        console.log("This is not a valid date format.")
+    } 
+  };
+  
+  // let date = new Date();
+  getDate();
+  
+  document.getElementById("btn").addEventListener("click", () => {
+    validate();
+  
+  }); 
